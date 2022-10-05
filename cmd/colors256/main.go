1798302749e6ce2e22c20141f3723760c7cc8fe8
Yongle Xu <yonglexu08@gmail.com>
2022-10-05 22:03:01 +0000

    256 color output added

---
+++ cmd/colors256/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rwx-yxu/go-term"
+
+func main() {
+	term.Colors256()
+}
--- term.go
+++ term.go
@@ -14,3 +14,15 @@ func TextBrightColors() {
 	fmt.Printf("%v A %v B %v C %v D %v E %v F %v G %v H %v \n", sequence.BBlack, sequence.BRed, sequence.BGreen, sequence.BYellow, sequence.BBlue, sequence.BMegenta, sequence.BCyan, sequence.BWhite, sequence.Reset)
 
 }
+
+func Colors256() {
+	for i := 0; i < 16; i++ {
+		for j := 0; j < 16; j++ {
+			code := i*16 + j
+			fmt.Printf("\033[38;5;%vm %v", code, code)
+		}
+		fmt.Print("\n")
+	}
+	fmt.Println(sequence.Reset)
+
+}
