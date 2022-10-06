a783417016711bc8b969f72d14ac17c8b89582bc
Yongle Xu <yonglexu08@gmail.com>
2022-10-06 13:12:46 +0000

    Provide error message if sample command is ran without any args

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/rwx-yxu/go-term/sample"
 )
 
 func main() {
-	if os.Args[1] != "" {
+	if len(os.Args) == 1 {
 
-		sample.Run(os.Args[1])
+		log.Println("Please rerun program with a sample command argument.")
+		return
 	}
+
+	sample.Run(os.Args[1])
+
 }
